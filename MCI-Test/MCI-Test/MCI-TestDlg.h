31002bd32ea90afe56458bf993aa0f345dbb7441
Keyur1876 <s85495@htw-dresden.de>
2025-10-23 20:37:39 +0200

    Add project files.

---
+++ MCI-Test/MCI-Test/MCI-TestDlg.h
@@ -0,0 +1,34 @@
+
+// MCI-TestDlg.h : header file
+//
+
+#pragma once
+
+
+// CMCITestDlg dialog
+class CMCITestDlg : public CDialogEx
+{
+// Construction
+public:
+	CMCITestDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MCITEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
